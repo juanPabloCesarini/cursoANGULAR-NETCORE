d3586732b9fe6c7e928e55931bf33a65ba6239b6
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-03-28 17:29:58 -0300

    Listado de tarjetas a partir de un array

--- angular/FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ angular/FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
-
+  listTarjetas: any[] = [
+    { titular: "Juan Perez", nroTarj: "5435587756", fechaExp: "11/21", cvv: "123"},
+    { titular: "Maia Juarez", nroTarj: "376625555225489", fechaExp: "04/23", cvv: "548"},
+  ];
   constructor() { }
 
   ngOnInit(): void {
