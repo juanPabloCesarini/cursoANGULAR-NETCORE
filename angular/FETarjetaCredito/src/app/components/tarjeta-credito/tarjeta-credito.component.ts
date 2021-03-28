c47e4fc54ee1cd1bfa1b79a945a4bcad938156a1
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-03-28 18:06:35 -0300

    Importando modulo para trabajar el form. En este caso el ReactiveFormsModule

--- angular/FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ angular/FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -10,7 +11,17 @@ export class TarjetaCreditoComponent implements OnInit {
     { titular: "Juan Perez", nroTarj: "5435587756", fechaExp: "11/21", cvv: "123"},
     { titular: "Maia Juarez", nroTarj: "376625555225489", fechaExp: "04/23", cvv: "548"},
   ];
-  constructor() { }
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: [''],
+      nroTarj: [''],
+      fechaExp: [''],
+      cvv: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
