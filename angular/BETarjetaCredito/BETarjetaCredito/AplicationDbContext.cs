73bb8e7a6bb78c054a909bdd682e1f35bedeb2e8
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-03-31 21:55:53 -0300

    Generando la primer migración a la database

---
+++ angular/BETarjetaCredito/BETarjetaCredito/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BETarjetaCredito.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETarjetaCredito
+{
+    public class AplicationDbContext: DbContext
+    {
+        
+        DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options) 
+        {
+        
+        }
+    }
+}
